a3e9c32e3d65536cec6aa47e999b01610371faee
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-15 21:25:24 -0700

    cleanup

--- main.tf
+++ main.tf
@@ -4,15 +4,13 @@ module "global" {
   source = "../module-aws-global"
 
   bucket_remote_state = "${var.bucket_remote_state}"
-  context_org = "${var.context_org}"
-
-  az_count = "${var.az_count}"
   az_names = "${var.az_names}"
+  az_count = "${length(split(" ",var.az_names))}"
 }
 
 output "bucket_remote_state" {
-    value = "${module.global.bucket_remote_state}"
-  }
+  value = "${module.global.bucket_remote_state}"
+}
 
 output "az_count" {
   value = "${module.global.az_count}"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,2 @@
-provider_region = "us-west-1"
-
 bucket_remote_state = "imma-remote-state"
-
-az_count = 2
 az_names = "us-west-1b us-west-1c"
