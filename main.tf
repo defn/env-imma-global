9f4403addb0b5de2a0b7a6d0ff17740dc7e6e81e
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-15 20:13:26 -0700

    dedent

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ output "bucket_remote_state" {
     value = "${module.global.bucket_remote_state}"
   }
 
-  output "az_count" {
-    value = "${module.global.az_count}"
-  }
+output "az_count" {
+  value = "${module.global.az_count}"
+}
 
-  output "az_names" {
-    value = "${module.global.az_names}"
-  }
+output "az_names" {
+  value = "${module.global.az_names}"
+}
