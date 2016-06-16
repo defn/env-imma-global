8b55d617087fd38b99b4a718ea7c78a283ad506e
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-15 23:00:15 -0700

    getting ready to split common tf to fogg

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-provider "aws" { }
-
 variable "az_names" {
   default = [ "us-west-1b", "us-west-1c" ]
-}
-
-module "global" {
-  source = "../module-aws-global"
-
-  bucket_remote_state = "${var.bucket_remote_state}"
-  az_names = "${var.az_names}"
-}
-
-output "bucket_remote_state" {
-  value = "${module.global.bucket_remote_state}"
-}
-
-output "az_count" {
-  value = "${module.global.az_count}"
-}
-
-output "az_names" {
-  value = "${module.global.az_names}"
 }
